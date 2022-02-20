7652fbc4d6371be22146515fcc64bc5a6e293f47
muralitharan <muralitharan805@gmail.com>
2022-02-20 17:43:20 +0530

    cors

--- server/server.ts
+++ server/server.ts
@@ -1,27 +1,21 @@
-
-
-import * as express from 'express';
-import {Application} from "express";
-import {getAllCourses, getCourseById} from "./get-courses.route";
-import {searchLessons} from "./search-lessons.route";
-
+import * as express from "express";
+import { Application } from "express";
+import { getAllCourses, getCourseById } from "./get-courses.route";
+import { searchLessons } from "./search-lessons.route";
+var cors = require("cors");
 
 const app: Application = express();
+app.use(cors());
 
+app.route("/api/courses").get(getAllCourses);
 
-app.route('/api/courses').get(getAllCourses);
+app.route("/api/courses/:id").get(getCourseById);
 
-app.route('/api/courses/:id').get(getCourseById);
+app.route("/api/lessons").get(searchLessons);
 
-app.route('/api/lessons').get(searchLessons);
-
-
-
-
-const httpServer:any = app.listen(9000, () => {
-    console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);
+const httpServer: any = app.listen(9000, () => {
+  console.log(
+    "HTTP REST API Server running at http://localhost:" +
+      httpServer.address().port
+  );
 });
-
-
-
-
