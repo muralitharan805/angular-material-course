9e6b8b25a7652ad13c53b7a05b1d08e594b6c1bb
muralitharan <muralitharan805@gmail.com>
2022-04-01 07:13:30 +0530

    table datasource with backend data

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -17,8 +17,10 @@ import {
   startWith,
   tap,
   delay,
+  catchError,
 } from "rxjs/operators";
-import { merge, fromEvent } from "rxjs";
+import { merge, fromEvent, throwError } from "rxjs";
+import { Lesson } from "../model/lesson";
 
 @Component({
   selector: "course",
@@ -28,85 +30,7 @@ import { merge, fromEvent } from "rxjs";
 export class CourseComponent implements OnInit, AfterViewInit {
   course: Course;
 
-  lessons = [
-    {
-      id: 120,
-      description: "Introduction to Angular Material",
-      duration: "4:17",
-      seqNo: 1,
-      courseId: 11,
-    },
-    {
-      id: 121,
-      description: "Navigation and Containers",
-      duration: "6:37",
-      seqNo: 2,
-      courseId: 11,
-    },
-    {
-      id: 122,
-      description: "Data Tables",
-      duration: "8:03",
-      seqNo: 3,
-      courseId: 11,
-    },
-    {
-      id: 123,
-      description: "Dialogs and Overlays",
-      duration: "11:46",
-      seqNo: 4,
-      courseId: 11,
-    },
-    {
-      id: 124,
-      description: "Commonly used Form Controls",
-      duration: "7:17",
-      seqNo: 5,
-      courseId: 11,
-    },
-    {
-      id: 125,
-      description: "Drag and Drop",
-      duration: "8:16",
-      seqNo: 6,
-      courseId: 11,
-    },
-    {
-      id: 126,
-      description: "Responsive Design",
-      duration: "7:28",
-      seqNo: 7,
-      courseId: 11,
-    },
-    {
-      id: 127,
-      description: "Tree Component",
-      duration: "11:09",
-      seqNo: 8,
-      courseId: 11,
-    },
-    {
-      id: 128,
-      description: "Virtual Scrolling",
-      duration: "3:44",
-      seqNo: 9,
-      courseId: 11,
-    },
-    {
-      id: 129,
-      description: "Custom Themes",
-      duration: "8:55",
-      seqNo: 10,
-      courseId: 11,
-    },
-    {
-      id: 130,
-      description: "Changing Theme at Runtime",
-      duration: "12:37",
-      seqNo: 11,
-      courseId: 11,
-    },
-  ];
+  lessons: Lesson[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -117,6 +41,22 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.course = this.route.snapshot.data["course"];
+    this.loadLessonsPage();
+  }
+
+  loadLessonsPage() {
+    this.coursesService
+      .findLessons(this.course.id, "asc", 0, 3)
+      .pipe(
+        tap((lessons) => (this.lessons = lessons)),
+        catchError((err) => {
+          console.log("err loading lessons api", err);
+
+          alert("err loading lessons api");
+          return throwError(err);
+        })
+      )
+      .subscribe();
   }
 
   ngAfterViewInit() {}
