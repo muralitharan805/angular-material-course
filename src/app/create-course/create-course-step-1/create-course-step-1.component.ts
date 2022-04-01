c3e25d7ed16e6e0c725f85600cf784c0d51f4184
muralitharan <muralitharan805@gmail.com>
2022-04-01 06:46:20 +0530

    forms

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,33 +1,27 @@
-import {Component} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
-
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "create-course-step-1",
-  templateUrl:"create-course-step-1.component.html",
-  styleUrls: ["create-course-step-1.component.scss"]
+  templateUrl: "create-course-step-1.component.html",
+  styleUrls: ["create-course-step-1.component.scss"],
 })
 export class CreateCourseStep1Component {
-
   form = this.fb.group({
-    title: ['', [
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(60)
-    ]],
-    releasedAt: [new Date(), Validators.required],
-    category: ['BEGINNER', Validators.required],
-    courseType: ['premium', Validators.required],
+    title: [
+      "",
+      [Validators.required, Validators.minLength(5), Validators.maxLength(60)],
+    ],
+    releasedAt: [new Date(1990, 0, 1), Validators.required],
+    category: ["BEGINNER", Validators.required],
+    courseType: ["premium", Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: ["", [Validators.required, Validators.minLength(3)]],
   });
 
-  constructor(private fb: FormBuilder) {
-
-  }
+  constructor(private fb: FormBuilder) {}
 
   get courseTitle() {
-    return this.form.controls['title'];
+    return this.form.controls["title"];
   }
-
 }
