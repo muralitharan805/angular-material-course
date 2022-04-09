0630af7af60721e3517d0845bd60dd57ebaf10bc
muralitharan <muralitharan805@gmail.com>
2022-04-09 19:01:38 +0530

    table check box

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -22,6 +22,7 @@ import {
 } from "rxjs/operators";
 import { merge, fromEvent, throwError } from "rxjs";
 import { Lesson } from "../model/lesson";
+import { SelectionModel } from "@angular/cdk/collections";
 
 @Component({
   selector: "course",
@@ -39,12 +40,34 @@ export class CourseComponent implements OnInit, AfterViewInit {
   lessons: Lesson[] = [];
   loading = false;
 
+  selection = new SelectionModel<Lesson>(true, []);
+
   constructor(
     private route: ActivatedRoute,
     private coursesService: CoursesService
   ) {}
 
-  displayedColumns = ["seqNo", "description", "duration"];
+  isAllSelected() {
+    return this.selection.selected.length == this.lessons.length;
+  }
+  onLessonSelected(lessons: Lesson) {
+    this.selection.toggle(lessons);
+
+    console.log(this.selection.selected);
+
+    // this.selection.isSelected;
+  }
+
+  toggleAll() {
+    // console.log("clicked");
+
+    if (this.isAllSelected()) {
+      this.selection.clear();
+    } else {
+      this.selection.select(...this.lessons);
+    }
+  }
+  displayedColumns = ["select", "seqNo", "description", "duration"];
 
   ngOnInit() {
     this.course = this.route.snapshot.data["course"];
