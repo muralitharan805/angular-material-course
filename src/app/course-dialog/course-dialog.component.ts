e6dc76138aaa5d515abd343072a9ceea6ab574cb
muralitharan <muralitharan805@gmail.com>
2022-04-09 21:23:02 +0530

    dialog

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,24 +1,52 @@
-import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import * as moment from 'moment';
+import { Component, Inject, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogConfig,
+  MatDialogRef,
+} from "@angular/material/dialog";
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import * as moment from "moment";
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: "course-dialog",
+  templateUrl: "./course-dialog.component.html",
+  styleUrls: ["./course-dialog.component.css"],
 })
 export class CourseDialogComponent implements OnInit {
+  description;
+  constructor(
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) private course: Course,
+    private dialogRef: MatDialogRef<CourseDialogComponent>
+  ) {}
+  form = this.fb.group({
+    description: ["", Validators.required],
+    category: ["BEGINNER", Validators.required],
+    releasedAt: [new Date(), Validators.required],
+    longDescription: [this.course.longDescription, Validators.required],
+  });
+  close() {
+    this.dialogRef.close();
+  }
 
-    constructor(private fb: FormBuilder) {
-
-
-    }
-
-    ngOnInit() {
-
-    }
-
+  save() {
+    this.dialogRef.close(this.form.value);
+  }
+  ngOnInit() {}
 }
 
+export function openCourseDialog(dialog: MatDialog, course: Course) {
+  const config = new MatDialogConfig();
+
+  config.disableClose = true;
+  config.autoFocus = true;
+
+  config.data = {
+    ...course,
+  };
+
+  const dialogConfig = dialog.open(CourseDialogComponent, config);
+  return dialogConfig.afterClosed();
+}
--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,36 +1,27 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
+import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { openCourseDialog } from "../course-dialog/course-dialog.component";
+import { filter } from "rxjs/operators";
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+  selector: "courses-card-list",
+  templateUrl: "./courses-card-list.component.html",
+  styleUrls: ["./courses-card-list.component.css"],
 })
 export class CoursesCardListComponent implements OnInit {
+  @Input()
+  courses: Course[];
 
-    @Input()
-    courses: Course[];
+  constructor(private dialog: MatDialog) {}
 
-    constructor() {
-    }
+  ngOnInit() {}
 
-    ngOnInit() {
-
-    }
-
-    editCourse(course:Course) {
-
-
-    }
-
+  editCourse(course: Course) {
+    openCourseDialog(this.dialog, course)
+      .pipe(filter((val) => !!val))
+      .subscribe((val) => {
+        console.log("new Course value", val);
+      });
+  }
 }
-
-
-
-
-
-
-
-
-
